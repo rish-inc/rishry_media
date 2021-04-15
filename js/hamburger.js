d5f2cbfffb1956034dac2bf8ca9d90b520afe2e1
ueharamiu <ueharamiu66@gmail.com>
2021-04-15 12:41:03 +0900

    ハンバーガーメニュー 完了

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,5 +2,6 @@ jQuery( function( $ ) {
     $( ".p-hamburger-menu" ).on( "click", function() {
     $( this ).toggleClass( "is-open" );
     $( "body" ).toggleClass( "is-open" );
+    $( ".c-dorower-bg-sp" ).toggleClass( "is-open" );
     } );
     } );
