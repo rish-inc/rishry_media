0c2cd6c157cee0fba004c8871e9f1b5b79e083f5
YAT <yat@yat-net.com>
2021-10-23 14:22:13 +0900

    deploy

--- .github/workflows/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# srcファイルをrsync over SSHで転送
-rsync -acvz --delete / yat8823jp@153.126.179.195:project/
-# サービスを再起動
-ssh test@your.ip.address "cd project/; docker-compose build; docker-compose down; docker-compose -f docker-compose.yml -f docker-compose-prod.yml up -d"
