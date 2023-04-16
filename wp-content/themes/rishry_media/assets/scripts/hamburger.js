4747884b7fab853936b1737a62980473048aae1c
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-04-16 20:51:17 +0900

    Update: ハンバーガーボタンの機能修正

--- wp-content/themes/rishry_media/assets/scripts/hamburger.js
+++ wp-content/themes/rishry_media/assets/scripts/hamburger.js
@@ -2,18 +2,24 @@ const hamburger = document.querySelector( ".js-hamburger" );
 const headNav   = document.querySelector( ".p-head-nav" );
 const body      = document.querySelector( "body" );
 const dorower   = document.querySelector( ".c-dorower-bg-sp" );
+const logo      = document.querySelector(".c-header-flex-beside__logo");
 
 hamburger.addEventListener( "click", () => {
   hamburger.classList.toggle( "is-open" );
   headNav.classList.toggle( "active" );
   body.classList.toggle( "is-open" );
   dorower.classList.toggle( "is-open" );
+  logo.classList.toggle( "is-open" );
 
-  // window.addEventListener("resize", () => {
-  //   if (window.innerWidth >= 1025 ) {
-  //     hamburger.classList.remove("is-open");
-  //     headNav.classList.remove("active");
-  //     body.classList.remove("is-open");
-  //   }
-  // });
+  if(window.matchMedia( "(min-width: 600px)" ).matches) {
+    dorower.classList.remove( "is-open" );
+  }
+});
+
+window.addEventListener("resize", () => {
+  hamburger.classList.remove( "is-open" );
+  headNav.classList.remove( "active" );
+  body.classList.remove( "is-open" );
+  dorower.classList.remove( "is-open" );
+  logo.classList.remove( "is-open" );
 });
