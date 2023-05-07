2bffbd20099380c4bae532117dd63b65ca850e15
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-05-07 14:05:02 +0900

    Fix: 検索フォーム関連の修正

--- wp-content/themes/rishry_media/search.php
+++ wp-content/themes/rishry_media/search.php
@@ -1,27 +1,31 @@
 <?php get_header(); ?>
-		<div class="l-main">
-			<div class="p-search">
-				<div class="p-search__window">
-					<?php get_search_form(); ?>
-				</div>
+	<div class="l-main">
+		<div class="p-search">
+			<div class="p-search__window">
+				<?php get_search_form(); ?>
 			</div>
-			<main class="p-contents">
-				<article class="p-detail__search">
-					<p class="p-detail__search__results"><?php the_search_query(); ?>の検索結果</p>
-						<?php get_template_part( "assets/components/card_search" ); ?>
-				</article>
-				<?php
-					$args = array (
-						'prev_text'          => '前へ',
-						'next_text'          => '次へ',
-						'screen_reader_text' => "Posts navigation",
-						'show_all'           => false,
-						'mid_size'           => 0,
-					);
-					the_posts_pagination( $args );
-				?>
-			</main>
-			<?php get_sidebar(); ?>
 		</div>
+		<main class="p-contents">
+			<article class="p-detail__search">
+				<?php if (empty(get_search_query())) : ?>
+					<p class="not-entered">「検索キーワードが未入力です」</p>
+				<?php else : ?>
+				<p class="p-detail__search__results"><?php the_search_query(); ?>の検索結果</p>
+					<?php get_template_part( "assets/components/card_search" ); ?>
+			</article>
+			<?php
+				$args = array (
+					'prev_text'          => '前へ',
+					'next_text'          => '次へ',
+					'screen_reader_text' => "Posts navigation",
+					'show_all'           => false,
+					'mid_size'           => 0,
+				);
+				the_posts_pagination( $args );
+			?>
+		</main>
+		<?php get_sidebar(); ?>
+		<?php endif; ?>
+	</div>
 
 <?php get_footer(); ?>
