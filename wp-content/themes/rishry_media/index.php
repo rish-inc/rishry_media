688fdbac3a2901ef37a2513d15e1299a5d3c2c4a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-05-07 09:21:34 +0900

    Update: メイン画像にサイトタイトルの表示

--- wp-content/themes/rishry_media/index.php
+++ wp-content/themes/rishry_media/index.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
-<div class="l-contents">
+<article class="l-contents">
 	<div class="p-search">
 		<div class="p-search__window">
 			<?php get_search_form(); ?>
 		</div>
 	</div>
-	<div class="p-mainvisual"><img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/dummy.jpg' ) ); ?>" alt="メインビジュアル　ダミー画像"></div>
+	<div class="p-mainvisual"><img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/dummy.jpg' ) ); ?>" alt="メインビジュアル　ダミー画像"><p class="p-mainvisual__title">ファッション＆メイク初心者さんのための情報サイト♡</p></div>
 	<main class="p-main">
-		<article class="p-detail">
+		<section class="p-detail">
 			<h2 class="c-title c-headline-border">MEDIA</h2>
 				<?php get_template_part( "assets/components/card" ); ?>
-		</article>
+		</section>
 		<?php
 			$args = array (
 				'prev_text'          => '前へ',
@@ -23,5 +23,5 @@
 		?>
 	</main>
 	<?php get_sidebar(); ?>
-</div>
+</article>
 <?php get_footer(); ?>
