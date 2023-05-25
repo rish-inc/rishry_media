55ec7bb122eb4b514e08c2766ff07cdb7aeb1924
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-05-26 08:57:21 +0900

    Update: single.phpの各種スタイリングの調整

--- wp-content/themes/rishry_media/dist/assets/main.js
+++ wp-content/themes/rishry_media/dist/assets/main.js
@@ -1,13 +1,10 @@
 const sideNav = document.querySelector(".p-side-navigation");
-const pagination = document.querySelector(".c-pagination");
-const detailSearch = document.querySelector(".p-detail__search");
+document.querySelector(".c-pagination");
+document.querySelector(".p-detail__search");
 window.addEventListener("load", function() {
   document.body.style.visibility = "visible";
   if (document.getElementById("search_result") != null) {
     sideNav.classList.add("is-close");
-  }
-  if (!document.body.contains(pagination)) {
-    detailSearch.style.marginBottom = "40px";
   }
 });
 const hamburger = document.querySelector(".js-hamburger");
