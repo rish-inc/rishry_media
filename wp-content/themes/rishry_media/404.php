d99478e7823c79f138fd9664d7ed0b0ef61ebb85
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-06-01 10:42:30 +0900

    Update: 404.phpのWordPress化とコーディング

--- wp-content/themes/rishry_media/404.php
+++ wp-content/themes/rishry_media/404.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
-		<main class="l-main">
-			<div class="p-search">
+	<main class="l-main">
+		<div class="p-search">
+			<div class="p-search__window">
 				<?php get_search_form(); ?>
 			</div>
-			<article class="p-contents">
-				<section class="p-detail error">
-					<h2 class="error__title">404 File not found.</h2>
-					<h3>お探しのページはありません</h3>
-					<p>お探しのページは移動、または削除された可能性があります。</p>
-					<a class="back-to-top c-btn" href="/">TOPへ戻る</a>
-				</section>
-			</article>
-			<?php get_sidebar(); ?>
-		</main>
+		</div>
+		<article class="p-contents_404">
+			<section class="p-detail error">
+				<h2 class="error__title">404 File not found.</h2>
+				<h3>お探しのページはありません</h3>
+				<p>お探しのページは移動、または削除された可能性があります。</p>
+				<a class="back-to-top c-btn" href="<?php echo esc_url(  home_url() ); ?>" >トップへ戻る</a>
+			</section>
+		</article>
+		<?php get_sidebar(); ?>
+	</main>
 <?php get_footer(); ?>
