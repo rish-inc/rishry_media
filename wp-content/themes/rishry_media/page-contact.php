4c8f52e2e02db2a0db11c661bc03a3f4eb788deb
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-06-05 19:49:30 +0900

    Update: page-contact.phpのWordPress化とコーディング

--- wp-content/themes/rishry_media/page-contact.php
+++ wp-content/themes/rishry_media/page-contact.php
@@ -1,52 +1,20 @@
 <?php get_header(); ?>
-		<main class="l-main">
+		<div class="l-contents">
 			<div class="p-search">
-				<?php get_search_form(); ?>
+				<div class="p-search__window">
+					<?php get_search_form(); ?>
+				</div>
 			</div>
 			<article class="p-contents">
-				<section class="p-detail p-contact">
-					<h1 class="c-title c-headline-border">お問い合わせ</h1>
-					<form class="p-contact__form" action="#">
-						<dl>
-							<dt>
-								<label for="name">お名前<span class="mandatory-tag">必須</span></label>
-							</dt>
-							<dd>
-								<input type="text" id="name" name="your-name">
-							</dd>
-						</dl>
-						<dl>
-							<dt>
-								<label for="email">メールアドレス<span class="mandatory-tag">必須</span></label>
-							</dt>
-							<dd>
-								<input type="text" id="email" name="your-email">
-							</dd>
-						</dl>
-						<dl>
-							<dt>
-								<label for="f_title">題名</label>
-							</dt>
-							<dd>
-								<input type="text" id="f_title" name="your-f_title">
-							</dd>
-						</dl>
-						<dl>
-							<dt>
-								<label for="message">メッセージ本文<span class="mandatory-tag">必須</span></label>
-							</dt>
-							<dd>
-								<textarea name="message" id="your-message"></textarea>
-							</dd>
-						</dl>
-						<div>
-							<input type="submit" class="button" value="送信">
-						</div>
-					</form>
+				<section class="p-detail__contact p-contact">
+					<h1 class="c-title__contact c-headline-border">お問い合わせ</h1>
+					<?php if (have_posts()) : ?>
+						<?php while (have_posts()) : the_post(); ?>
+							<?php the_content(); ?>
+						<?php endwhile; ?>
+					<?php endif; ?>
 				</section>
 			</article>
-
 			<?php get_sidebar(); ?>
-		</main>
-
+		</div>
 <?php get_footer(); ?>
