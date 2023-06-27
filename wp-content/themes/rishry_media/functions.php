c7602e2a2c3c9afc286f44c6d75ad7d79f3e2cee
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-06-27 19:52:26 +0900

    Fix: add_editor_styleの追加

--- wp-content/themes/rishry_media/functions.php
+++ wp-content/themes/rishry_media/functions.php
@@ -255,3 +255,8 @@ add_action( 'init', function()  {
 			)
 		);
 	});
+
+function wpdocs_theme_add_editor_styles() {
+	add_editor_style( 'custom-editor-style.css' );
+}
+add_action( 'admin_init', 'wpdocs_theme_add_editor_styles' );
