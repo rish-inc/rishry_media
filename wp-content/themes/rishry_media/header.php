6fde1c99079cf1eb1a1e0662ef50d4a7d467e964
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-07-20 21:03:43 +0900

    Fix: ハンバーガーボタン展開時のTwitterアイコンのリンク修正及び不要部分のコメントアウト

--- wp-content/themes/rishry_media/header.php
+++ wp-content/themes/rishry_media/header.php
@@ -40,9 +40,17 @@
 						</nav>
 						<nav class="p-head-nav__sns">
 							<ul class="p-head-nav__icon c-sns__icon">
-								<li class="twitter"><a href="#"><img src="<?php echo esc_url( get_theme_file_uri( '/dist/assets/images/t_logo.png' )); ?>" alt="twitter"></a></li>
+								<li class="twitter">
+									<a href="<?php echo esc_attr( ssam_url()['twitter'] ); ?>">
+										<img src="<?php echo esc_url( get_theme_file_uri( '/dist/assets/images/t_logo.png' )); ?>" 	alt="twitter">
+									</a>
+								</li>
+								<?php
+								/*
 								<li class="facebook"><a href="#"><img src="<?php echo esc_url( get_theme_file_uri( '/dist/assets/images/f_logo.png' )); ?>" alt="facebook"></a></li>
 								<li class="instagram"><a href="#"><img src="<?php echo esc_url( get_theme_file_uri( '/dist/assets/images/i_logo.png' )); ?>" alt="instagram"></a></li>
+								*/
+								?>
 							</ul>
 						</nav>
 					</div>
